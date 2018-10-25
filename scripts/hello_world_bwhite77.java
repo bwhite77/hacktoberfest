cad174ad987e425e321d3e107c40b56d13aa2036
bwhite77 <44416770+bwhite77@users.noreply.github.com>
2018-10-24 20:45:31 -0500

    Update scripts/hello_world_bwhite77.java

---
+++ scripts/hello_world_bwhite77.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java
+// AUTHOR: Ben White
+// GITHUB: https://github.com/bwhite77
+
+public class HelloWorld {
+  public static void main() {
+    System.out.println("Hello world!");
+  }
+}
